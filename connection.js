2846854e441c0790fd14a9b882b8c5c8aee5b7ef
NegaNe <manfireburst@gmail.com>
2022-12-04 10:50:16 +0700

    ganti

--- connection.js
+++ connection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://hansen:web@webprogramming.fboproe.mongodb.net/FTI21')
 
-const db = mongoose.connection;
-console.log('Connection is On')
+console.log('MongDB is Connected')
 
 
